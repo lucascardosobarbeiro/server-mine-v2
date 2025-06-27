12077eb3b0e0a9b1f823aa35dad43a8c429fdb25
Lucas Cardoso Barbeiro <lucas.barbeiro@educacao.sp.gov.br>
2025-06-27 16:56:03 -0300

    Feat: Adiciona ficheiros de configuração docker-compose e velocity

--- compute.tf
+++ compute.tf
@@ -51,7 +51,17 @@ resource "google_compute_instance" "minecraft_server_host" {
       mkdir -p /mnt/data
       mount -o discard,defaults /dev/sdb /mnt/data
       echo UUID=$(blkid -s UUID -o value /dev/sdb) /mnt/data ext4 discard,defaults,nofail 0 2 | tee -a /etc/fstab
-      # Instala o Docker
+      
+      # Instala o Docker e o plugin Compose
+      apt-get update
+      apt-get install -y ca-certificates curl
+      install -m 0755 -d /etc/apt/keyrings
+      curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+      chmod a+r /etc/apt/keyrings/docker.asc
+      echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        tee /etc/apt/sources.list.d/docker.list > /dev/null
       apt-get update
       apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     EOT
