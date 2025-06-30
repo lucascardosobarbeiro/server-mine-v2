c4464cee0f76b7991837245e335816adf86414fc
Lucas Cardoso Barbeiro <lucas.barbeiro@educacao.sp.gov.br>
2025-06-30 16:37:39 -0300

    test: validate new provider

--- terraform/github_actions.tf
+++ terraform/github_actions.tf
@@ -1,31 +1,46 @@
-# github_actions.tf - Versão Final e Limpa
+resource "random_string" "suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
 
-# 1. Cria um "Pool de Identidades"
 resource "google_iam_workload_identity_pool" "github_pool" {
   workload_identity_pool_id = "github-pool-${random_string.suffix.result}"
   display_name              = "GitHub Actions Pool"
+  project                   = var.project_id
 }
 
-# 2. Cria um "Provedor" dentro do Pool
 resource "google_iam_workload_identity_pool_provider" "github_provider" {
+  project                            = var.project_id
   workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-provider"
   display_name                       = "GitHub OIDC Provider"
-  attribute_condition                = "attribute.repository == '${var.github_repo}'"
 
   attribute_mapping = {
     "google.subject"       = "assertion.sub"
     "attribute.actor"      = "assertion.actor"
     "attribute.repository" = "assertion.repository"
+    "attribute.ref"        = "assertion.ref"
   }
+
+  attribute_condition = "attribute.repository == \"${var.github_repo}\" && attribute.ref == \"refs/heads/main\""
+
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
 }
 
-# Recurso auxiliar para gerar o sufixo aleatório.
-resource "random_string" "suffix" {
-  length  = 4
-  special = false
-  upper   = false
+resource "google_service_account" "minecraft_sa" {
+  account_id   = "sa-minecraft-vm"
+  display_name = "SA for Minecraft VM"
+  project      = var.project_id
+}
+
+resource "google_service_account_iam_binding" "workload_identity_binding" {
+  service_account_id = google_service_account.minecraft_sa.name
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${var.github_repo}"
+  ]
 }
