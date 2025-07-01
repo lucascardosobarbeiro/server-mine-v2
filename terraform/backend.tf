1651ac6d2c32f8162e628799ccdf3d1529eadd13
Lucas Barbeiro <lcb.barbeiro@gmail.com>
2025-07-01 20:49:10 -0300

    git ignore final

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "terraform-state-server-mine-463823" # O nome do bucket que acabámos de criar.
-    prefix  = "terraform/state"                   # Um "caminho" dentro do bucket para organizar os ficheiros.
-  }
-}
-# Atenção: Este bloco é necessário para que o Terraform saiba onde guardar o estado.
-# O bucket deve existir antes de executar o Terraform pela primeira vez.
