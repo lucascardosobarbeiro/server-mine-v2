9f2ae1409950e497ba957fc1bd94fa35293b5e54
Lucas Barbeiro <lcb.barbeiro@gmail.com>
2025-07-12 13:24:07 -0300

    adicionando workflows

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,18 @@
-terraform {
+/*terraform {
   backend "gcs" {
     bucket  = "state-tf-minecraft"        # O bucket deve existir
     prefix  = "terraform/state"
+  }
+} */
+
+variable "state_prefix" {
+  type    = string
+  default = "terraform/state"
+}
+
+terraform {
+  backend "gcs" {
+    bucket = var.backend_bucket
+    prefix = var.state_prefix
   }
 }
