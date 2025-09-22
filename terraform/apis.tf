400211068509311fe6fe1760665354ea26250f48
Lucas Barbeiro <lcb.barbeiro@gmail.com>
2025-09-22 19:53:15 -0300

    novas alterações

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -44,22 +44,3 @@ resource "google_project_service" "cloudresourcemanager" {
   service = "cloudresourcemanager.googleapis.com"
 }
 
-resource "google_project_service" "cloudresourcemanager" {
-  project = var.project_id
-  service = "cloudresourcemanager.googleapis.com"
-}
-resource "google_project_service" "iam" {
-  service            = "iam.googleapis.com"
-  disable_on_destroy = false
-  depends_on = [
-    google_project_service.cloudresourcemanager
-  ]
-}
-
-resource "google_project_service" "iamcredentials" {
-  service            = "iamcredentials.googleapis.com"
-  disable_on_destroy = false
-  depends_on = [
-    google_project_service.cloudresourcemanager
-  ]
-}
